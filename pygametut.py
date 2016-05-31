789def9476a368757feb67ec45dfb9e658286361
George Goranov <g.p.goranov@gmail.com>
2016-05-31 14:46:09 +0300

    add event

--- pygametut.py
+++ pygametut.py
@@ -3,7 +3,14 @@ import pygame
 pygame.init()
 
 game_display = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Slither')
 pygame.display.update()
+
+game_exit = False
+
+while not game_exit:
+    for event in pygame.event.get():
+        print(event)
 
 pygam.quit()
 quit()
