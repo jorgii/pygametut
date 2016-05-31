9e870fd5538b6b45dc7fecd320bb48a79a475dd2
George Goranov <g.p.goranov@gmail.com>
2016-05-31 16:14:49 +0300

    add block change var

--- pygametut.py
+++ pygametut.py
@@ -9,7 +9,7 @@ red = (255, 0, 0)
 display_width = 800
 display_height = 600
 
-game_display = pygame.display.set_mode((display_height, display_width))
+game_display = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Slither')
 
 game_exit = False
@@ -19,7 +19,8 @@ lead_y = display_height/2
 lead_x_change = 0
 lead_y_change = 0
 
-block_size = 2
+block_size = 10
+block_change = 2
 FPS = 100
 
 clock = pygame.time.Clock()
@@ -30,21 +31,21 @@ while not game_exit:
             game_exit = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                lead_x_change = -block_size
+                lead_x_change = -block_change
                 lead_y_change = 0
             elif event.key == pygame.K_RIGHT:
-                lead_x_change = block_size
+                lead_x_change = block_change
                 lead_y_change = 0
             elif event.key == pygame.K_UP:
-                lead_y_change = -block_size
+                lead_y_change = -block_change
                 lead_x_change = 0
             elif event.key == pygame.K_DOWN:
-                lead_y_change = block_size
+                lead_y_change = block_change
                 lead_x_change = 0
 
-    if lead_x >= (display_width - block_size) or \
+    if lead_x > (display_width - block_size) or \
             lead_x < 0 or \
-            lead_y >= (display_height - block_size) or \
+            lead_y > (display_height - block_size) or \
             lead_y < 0:
         game_exit = True
 
@@ -52,7 +53,10 @@ while not game_exit:
     lead_y += lead_y_change
 
     game_display.fill(white)
-    pygame.draw.rect(game_display, black, [lead_x, lead_y, 10, 10])
+    pygame.draw.rect(
+        game_display,
+        black,
+        [lead_x, lead_y, block_size, block_size])
 
     pygame.display.update()
 
