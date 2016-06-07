28df45711ccf5ebf4f10355e5bf37c26f832e540
George Goranov <g.p.goranov@gmail.com>
2016-06-07 14:47:57 +0300

    optimize pause display

--- pygametut.py
+++ pygametut.py
@@ -68,6 +68,17 @@ def message_to_screen(msg, color, y_displace=0, size='small'):
 
 def pause():
     paused = True
+    game_display.fill(white)
+    message_to_screen(
+        "Paused",
+        black,
+        -100,
+        size="large")
+    message_to_screen(
+        "Press C to continue or Q to quit.",
+        black,
+        25)
+    pygame.display.update()
     while paused:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -79,17 +90,6 @@ def pause():
                 elif event.key == pygame.K_q:
                     pygame.quit()
                     quit()
-        game_display.fill(white)
-        message_to_screen(
-            "Paused",
-            black,
-            -100,
-            size="large")
-        message_to_screen(
-            "Press C to continue or Q to quit.",
-            black,
-            25)
-        pygame.display.update()
         clock.tick(5)
 
 
